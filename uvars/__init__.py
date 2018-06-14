935ea81290c03cf156095c45b24a831fd911e44f
Mirmik <mirmikns@yandex.ru>
2018-06-14 14:44:58 +0300

    a

--- uvars/__init__.py
+++ uvars/__init__.py
@@ -7,12 +7,12 @@ def readfile(tgtdict, path):
 	with open(path) as of:
 		lines = of.readlines()
 		for idx, l in enumerate(lines):
-			splt = l.split()
+			splt = l.split('=')
 			if len(splt) != 2:
 				raise Exception("File format error. (file:{}, line:{})".format(path, idx))
 			if splt[0] in tgtdict:
 				raise Exception("Repeated key. (file:{}, key:{}, line:{})".format(path, splt[0], idx))	
-			tgtdict[splt[0]] = splt[1]
+			tgtdict[splt[0]] = splt[1][:-1]
 
 udict = {}
 udirs = {}
@@ -26,4 +26,24 @@ if os.path.exists(udirpath):
 		readfile(udirs[f], os.path.join(udirpath, f))
 
 print(udict)
-print(udirs)
+
+def update(dirf = None):
+	if dirf == None: 
+		path = ufilepath
+		dct = udict
+	else: 
+		if not dirf in udirs:
+			raise Exception("unexpected dir {}".format(dirf))
+		path = os.path.join(udirpath, dirf)
+		dct = udirs[dirf]
+	with open(path, "w") as f:
+		for k, v in dct.iteritems():
+			f.write("{}={}\n".format(k, v))
+
+def setvar(key, val, dirf = None):
+	if dirf: udirs[dirf][key] = val
+	else: udict[key] = val 
+	update(dirf)
+
+setvar("mirmik", "good boy")
+setvar("mirmik2", "good boy2")
