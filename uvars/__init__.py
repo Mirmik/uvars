fd8f4760685496276f86ec7fb61fc96352f4b719
Mirmik <mirmikns@yandex.ru>
2018-06-14 12:36:45 +0300

    a

--- uvars/__init__.py
+++ uvars/__init__.py
@@ -1,1 +1,6 @@
 version = "0.0.2"
+
+import uvars.variables
+
+print(uvars.variables.uvarsfile)
+print(uvars.variables.uvarsdirectory)
---
+++ uvars/variables.py
@@ -0,0 +1,4 @@
+import os
+
+uvarsfile = os.path.expanduser("~/.uvars")
+uvarsdirectory = os.path.expanduser("~/.uvars.d")
